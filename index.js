d7319d16c0bf4adae33dbb6646c46f213ed2bb23
Michael Cartee <admin@asgardcrolingai.lan>
2022-01-20 20:50:19 -0500

    up to step 4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your Code Here
+let firstAnswer = window.prompt('Do you head left or right?')
+console.log(firstAnswer==='left')
+if(firstAnswer === 'left'){    
+    let secondAnswer = window.prompt('You come across a stray cat. It scampers off down a small hole,
+just large enough for you to crawl through. Do you follow instanceof, or continue on your path?')
+
+} else if(firstAnswer === "right") {
+    let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead 
+    you away from the dragon altogether. Which path do you take?')
+    
+}
