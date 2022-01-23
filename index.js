f8701b01740662a22ebd5036932a097169ed30ca
Michael Cartee <admin@asgardcrolingai.lan>
2022-01-22 20:44:29 -0500

    Got the code working entirely, removed a typo for thirdAnswer, adding options for response

--- index.js
+++ index.js
@@ -2,28 +2,29 @@
 let firstAnswer = window.prompt('Do you head left or right?')
 console.log(firstAnswer==='left')
 if(firstAnswer === 'left'){    
-    let secondAnswer = window.prompt('You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through. Do you follow it, or continue on your path?')
+    let secondAnswer = window.prompt('You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through. Do you follow it, or continue on your path? Options are Follow or Continue')
     if(secondAnswer === 'Follow'){
-        let thirdAnswer = window.prompt('You Follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical sfe haven.')
+        let thirdAnswer = window.prompt('You Follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical sfe haven. Options are Stay or Spread')
         if(thirdAnswer === 'Stay') {
             window.alert ('You live happily amongst the cats for the rest of your days.')
-        }else if(thidAnswer === 'Follow') {
+        }else if(thirdAnswer === 'Spread') {
             window.alert ('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
         }
     }else if(secondAnswer ==='Continue'){
         let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
         if(thirdAnswer === 'Ladder') {
             window.alert ('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
-        }else if(thidAnswer === 'Staircase') {
+            console.log(thirdAnswer)
+        }else if(thirdAnswer === 'Stair') {
             window.alert ('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
         }
     }
 
     
 } else if(firstAnswer === "right") {
-    let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?')
+    let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take? Options are Past or Away')
     if(secondAnswer === "Past") {
-        let thirdAnswer = window.prompt('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+        let thirdAnswer = window.prompt('The dragon wakes up and sits upright. You only have a moment to respond, to Stay or Run:')
         if(thirdAnswer === "Stay"){
             window.alert ('You and the dragon have an upligting conversation about local politics and become lifelong friends.')
         } else if(thirdAnswer === "Run"){
